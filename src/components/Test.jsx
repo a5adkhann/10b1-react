82609ea96caa7c87c60d0c4bb2e3d664bba6c988
amna001 <amnaaslam7789@gmail.com>
2025-11-22 10:27:49 +0500

    ---

---
+++ src/components/Test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <>
+      <div className='bg-red-600 text-white font-bold text-9xl'>
+      Testing
+      </div>
+    </>
+  )
+}
+
+export default Test
